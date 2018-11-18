3b934f37d82d4dc73c84bc30e0fc0c16cd961182
StoneInside <StoneInside@PC>
2018-11-18 13:52:15 +0200

    add logging, loading system properties

---
+++ src/main/java/com/bilo/hosp/Config.java
@@ -0,0 +1,47 @@
+package com.bilo.hosp;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Config {
+    
+    private static final Logger LOG = LoggerFactory.getLogger(Config.class.getName());
+    private static volatile Config instance = null;
+    
+    private Config() {
+        try {
+            LOG.warn("Conf: load: acsk.properties = {}", System.getProperty("path.to.config"));
+            InputStream file = new FileInputStream(new File(System.getProperty("path.to.config")));
+            
+            Properties props = new Properties();
+            props.load(file);
+            
+            for (String key : props.stringPropertyNames()) {
+                String value = (String) props.getProperty(key);
+                
+                LOG.warn("SYSPROPS:" + key + "=" + value);
+                System.setProperty(key, value);                
+            }
+        } catch (Exception e) {
+            LOG.error("ERROR:" + e);
+            System.err.println("error" + e);
+        }
+    }
+    
+    public static Config getInstance() {
+        Config localInstance = instance;
+        if (localInstance == null) {
+            synchronized (Config.class) {
+                localInstance = instance;
+                if (localInstance == null) {
+                    instance = localInstance = new Config();
+                }
+            }
+        }
+        return localInstance;
+    }
+}
---
+++ src/main/java/com/bilo/hosp/Hosp.java
@@ -0,0 +1,9 @@
+package com.bilo.hosp;
+
+public class Hosp {
+   
+    public static void main(String[] args) {
+        Config conf = Config.getInstance();
+    }
+    
+}
