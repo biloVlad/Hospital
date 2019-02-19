a36ba1d7026ca6dd6b735b709f96e3397ab6cfa1
StoneInside <StoneInside@PC>
2019-02-19 21:24:53 +0200

    Revert "change set room identification"
    This reverts commit 1dcc7d568529f8f17dcdbe6c2aa1fd0b165fe42d.

--- src/main/java/com/bilo/hosp/service/SetRoomService.java
+++ src/main/java/com/bilo/hosp/service/SetRoomService.java
@@ -1,9 +1,9 @@
+
 package com.bilo.hosp.service;
 
 import com.bilo.hosp.DB;
 import com.bilo.hosp.controller.SetRoom;
 import com.mongodb.client.MongoCollection;
-import static com.mongodb.client.model.Filters.and;
 import static com.mongodb.client.model.Filters.eq;
 import static com.mongodb.client.model.Updates.set;
 import org.bson.Document;
@@ -13,39 +13,32 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class SetRoomService {
-
     private static final Logger LOG = LoggerFactory.getLogger(SetRoom.class.getName());
-
+    
     public SetRoomService() {
-
+       
     }
-
+    
     public String task(String buff, DB linkDB) {
         JSONObject result = new JSONObject();
         String collForAdd = System.getProperty("collectionForAddPatient");
         Document doc = Document.parse(buff);
 
         String _id = doc.getString("_id");
-        String pnum, pser;
-
-        pnum = doc.getString("pnum");
-        pser = doc.getString("pser");
-
         String room = doc.getString("room");
 
-        MongoCollection<Document> collection = linkDB.getDatabase().getCollection(collForAdd);
-       
+        MongoCollection<Document> collection = linkDB.getDatabase().getCollection(collForAdd); 
 
         Document targetPatient = collection.findOneAndUpdate(
-                and(eq("document.pnum", pnum), eq("document.pser", pser)),
+                eq("_id", new ObjectId(_id)),
                 set("room", room));
-        if (targetPatient != null) {
-            result.append("message", "Палата пациента '" + targetPatient.getString("name") + " " + targetPatient.getString("lastname") + "' изменена на " + room);
-            result.append("status", "success");
-        }
-
-        LOG.info(result.toString());
-
+         if(targetPatient != null) {    
+             result.append("message", "Палата пациента '" + targetPatient.getString("name") + " " + targetPatient.getString("lastname") + "' изменена на " + room);
+             result.append("status", "success");             
+         }
+        
+        LOG.info(result.toString());        
+        
         return result.toString();
     }
 }
