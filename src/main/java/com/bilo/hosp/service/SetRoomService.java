11a09c95a60965ffd52483721f1563dd8507428b
StoneInside <StoneInside@PC>
2019-02-19 21:27:01 +0200

    change set room identification

--- src/main/java/com/bilo/hosp/service/SetRoomService.java
+++ src/main/java/com/bilo/hosp/service/SetRoomService.java
@@ -4,6 +4,7 @@ package com.bilo.hosp.service;
 import com.bilo.hosp.DB;
 import com.bilo.hosp.controller.SetRoom;
 import com.mongodb.client.MongoCollection;
+import static com.mongodb.client.model.Filters.and;
 import static com.mongodb.client.model.Filters.eq;
 import static com.mongodb.client.model.Updates.set;
 import org.bson.Document;
@@ -24,13 +25,18 @@ public class SetRoomService {
         String collForAdd = System.getProperty("collectionForAddPatient");
         Document doc = Document.parse(buff);
 
-        String _id = doc.getString("_id");
+        String pnum, pser;
+        
+        pnum = doc.getString("pnum");
+        pser = doc.getString("pser");
+
         String room = doc.getString("room");
 
-        MongoCollection<Document> collection = linkDB.getDatabase().getCollection(collForAdd); 
+        MongoCollection<Document> collection = linkDB.getDatabase().getCollection(collForAdd);
+       
 
         Document targetPatient = collection.findOneAndUpdate(
-                eq("_id", new ObjectId(_id)),
+                and(eq("document.pnum", pnum), eq("document.pser", pser)),
                 set("room", room));
          if(targetPatient != null) {    
              result.append("message", "Палата пациента '" + targetPatient.getString("name") + " " + targetPatient.getString("lastname") + "' изменена на " + room);
